3fec17c97936eb8ff2b601ef47f119679f9bf05f
` Orchai Blyat! <84289524+Orchais@users.noreply.github.com>
2021-06-09 19:21:07 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,7 @@
-const Discord = require('discord.js');
 const config = require('../config.js');
 
 module.exports = client => {
 
 client.user.setPresence({ activity: { name:config.bot.ready }, status:'dnd', type:'playing' })
 
-};
-
-module.exports.config = {
-    name:'ready'
 };
